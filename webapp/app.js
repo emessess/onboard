8bbcc5044aa25fcb54e62de70f4c8c008620a145
Matthew Smith <msmith@NY-MB-002.local>
2018-01-19 14:43:06 -0500

    added Dockerfile for readline proj

--- webapp/app.js
+++ webapp/app.js
@@ -3,11 +3,13 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.use('*', (req, res, next) => {
   if (req.method !== 'GET') {
-    res.status(501).send('I don\'t do that.');
+    res.status(501).send('I don\'t do that.\n');
   }
   next();
 });
@@ -15,7 +17,6 @@ app.use('*', (req, res, next) => {
 app.get('/', (req, res) => {
   if (req.query.q) res.send(req.query.q);
   else res.send('OK.\n');
-  // console.log(req.method);
 });
 
 app.get('/:stuff', (req, res) => {
